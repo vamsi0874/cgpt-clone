3dbb63676d2ff645ad6faa9f79746d5fa13dd7b3
vamsi0874 <vamsim0874>
2025-03-11 22:26:59 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,7 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-console.log(__filename)
+
 
 
 
